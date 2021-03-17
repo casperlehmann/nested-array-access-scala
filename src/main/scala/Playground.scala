25ec3bd8cd0661e35cb17256930a1ea1a42ebcf6
casperlehmann <casperlehmann@gmail.com>
2021-03-17 22:55:25 +0100

    Init commit

---
+++ src/main/scala/Playground.scala
@@ -0,0 +1,30 @@
+object MultiplicationTable {
+  def main(args: Array[String]): Unit = {
+    println(getValueAtIndex(2, 3, 9))
+    println(getValueAtIndexForNDArray(multiTable, indices = 2, 3, 9))
+  }
+
+  def multiTable: Array[Array[Array[Int]]] = {
+    // Make a multiplication table, like in elementary school. Except it's 3D.
+    var multiTable = Array.ofDim[Int](10, 10, 10)
+    for (x <- 0 to 9; y <- 0 to 9; z <- 0 to 9) {
+      multiTable(x)(y)(z) = x * y * z
+    }
+    multiTable
+  }
+
+  def getValueAtIndex(x: Int, y: Int, z: Int): Int = {
+    multiTable(x)(y)(z)
+  }
+
+  def getValueAtIndexForNDArray[X](t: X, indices: Int*): Int = {
+    t match {
+      case t: Int => t: Int
+      case t: Array[_] => {
+        val rest = indices.drop(1)
+        val into = t(indices(0))
+        getValueAtIndexForNDArray(into, rest: _*)
+      }
+    }
+  }
+}
